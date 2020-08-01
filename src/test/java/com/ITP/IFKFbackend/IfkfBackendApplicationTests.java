84d9bdb6a0f4c2362ceaf30b4f6b4128f2659a5d
senath99 <senathweerasingha@gmail.com>
2020-08-02 02:55:34 +0530

    First Commit

---
+++ src/test/java/com/ITP/IFKFbackend/IfkfBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ITP.IFKFbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfkfBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
