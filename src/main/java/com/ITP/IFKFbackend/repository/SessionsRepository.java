13a11904c0576a12ff8d1d54d6edec2105da9bc3
Senath99 <senathweerasingha@gmail.com>
2020-09-16 12:10:41 +0530

    Senath's commit

---
+++ src/main/java/com/ITP/IFKFbackend/repository/SessionsRepository.java
@@ -0,0 +1,13 @@
+package com.ITP.IFKFbackend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ITP.IFKFbackend.model.Sessions;
+
+
+public interface SessionsRepository extends JpaRepository<Sessions, Long>{
+
+	
+	
+}
+
