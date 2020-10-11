751f82185f38226ffd93e84a270ba13df91bfb0d
Senath99 <senathweerasingha@gmail.com>
2020-10-11 10:32:05 +0530

    committing to resolve

--- src/main/java/com/ITP/IFKFbackend/repository/DonationsRepository.java
+++ src/main/java/com/ITP/IFKFbackend/repository/DonationsRepository.java
@@ -6,10 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import com.ITP.IFKFbackend.Dto.Donate_Session;
 import com.ITP.IFKFbackend.Dto.DonationSort;
 import com.ITP.IFKFbackend.model.Donations;
-import com.ITP.IFKFbackend.model.Rankings;
+
 
 
 
