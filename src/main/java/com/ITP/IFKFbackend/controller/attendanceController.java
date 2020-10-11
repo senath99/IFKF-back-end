9a7ece38cdcf38eb37be78172bba6a2372296cc8
AmayaKavirathne <amayakavi99@gmail.com>
2020-10-11 20:47:23 +0530

    Attendance insert,report added

--- src/main/java/com/ITP/IFKFbackend/controller/attendanceController.java
+++ src/main/java/com/ITP/IFKFbackend/controller/attendanceController.java
@@ -5,22 +5,27 @@ import java.util.Collection;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ITP.IFKFbackend.model.Attendance;
 import com.ITP.IFKFbackend.model.Payment;
+import com.ITP.IFKFbackend.model.Student;
 import com.ITP.IFKFbackend.repository.attendanceRepository;
 import com.ITP.IFKFbackend.service.AttendanceReport;
-import com.ITP.IFKFbackend.service.PaymentReport;
 
 import net.sf.jasperreports.engine.JRException;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3001")
+@CrossOrigin(origins = "http://localhost:3000")
 public class attendanceController {
 
 	@Autowired
@@ -39,15 +44,31 @@ public class attendanceController {
 //		return AttendanceRepository.findByattendanceID(studentId);
 //	}
 	
-	@GetMapping("/attendanceReports")
-	public String exportReport() throws FileNotFoundException, JRException {
-		return service.exportReport();
+	@PutMapping("/attendance/{Id}")
+	public ResponseEntity<Attendance> updateAttendance(
+			@PathVariable Long Id, @RequestBody Attendance att){
+		
+
+		Attendance result = AttendanceRepository.save(att);
+		
+		return new ResponseEntity<Attendance>(att, HttpStatus.OK);
 	}
 	
-
-//	@GetMapping("/attendance/search/{searchText}")
-//	public List<Attendance> searchStudent(@PathVariable  String searchText){
-//		return attendanceRepository.searchQuery(searchText);
-//	}
+	@PostMapping("/attendance")
+	public Attendance createStudent(@RequestBody Attendance att) {
+		
+		return AttendanceRepository.save(att);
+	}
+	
+	
+	@GetMapping("/attendanceBystu/{Id}")
+	 public Attendance getAttendanceByStu(@PathVariable String Id){
+		return AttendanceRepository.findByStuId(Id);
+	}
+	
+	@GetMapping("/attendance/report")
+	public String getReport() throws FileNotFoundException, JRException {
+		return service.exportReport();
+	}
 	
 }
--- src/main/java/com/ITP/IFKFbackend/repository/attendanceRepository.java
+++ src/main/java/com/ITP/IFKFbackend/repository/attendanceRepository.java
@@ -1,24 +1,15 @@
 package com.ITP.IFKFbackend.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.ITP.IFKFbackend.model.Attendance;
-import com.ITP.IFKFbackend.model.Payment;
 
 
-@Repository 
+
 public interface attendanceRepository extends JpaRepository<Attendance, Long>{
-//	public Attendance findTopByOrderBystuIdDesc();
-//	
-//	List<Attendance> findBystuId(String searchText);
-//	 
-//	 @Query("from Attendance a WHERE a.stuId = :searchText ORDER BY a.stuId")
-//	 List <Attendance> searchQuery (@Param("searchText") String stuId);
-
+	
+	
+	Attendance findByStuId(String stuID);
 
 }
